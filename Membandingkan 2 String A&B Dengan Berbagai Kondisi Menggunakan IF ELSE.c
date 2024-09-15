0f2f820b021eef790aac418f05f483736674b471
Mahadi Putra <mahadiputraananta@gmail.com>
2024-09-15 22:04:59 +0800

    Tugas Build IT

--- Membandingkan 2 String A&B Dengan Berbagai Kondisi Menggunakan IF ELSE.c
+++ Membandingkan 2 String A&B Dengan Berbagai Kondisi Menggunakan IF ELSE.c
@@ -12,17 +12,23 @@ int main()
 
     if (strlen(A) != strlen(B))
     {
+        printf("\n============\n");
         printf("BERBEDA\n");
+        printf("============\n");
     }
     else
     {
         if (strcmp(A, B) == 0)
         {
+            printf("\n============\n");
             printf("IDENTIK\n");
+            printf("============\n");
         }
         else
         {
+            printf("\n============\n");
             printf("MIRIP\n");
+            printf("============\n");
         }
     }
 
